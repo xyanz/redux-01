a8c26f3942f2af5ca3bec0c75e553dc63c47a59d
Yan Zhuk <yan.zhuk45@gmail.com>
2018-07-09 10:20:55 -0400

    add react-redux to connect react=>redux

---
+++ redux-basics.js
@@ -0,0 +1,25 @@
+const redux = require ('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+  counter: 0
+}
+
+// Reducer
+const rootReducer = (state = initialState, action) => {
+  return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});
+store.dispatch({type: 'ADD_COUNTER', value: 10});
+console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+  console.log('[Subscription]', store.getState());
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
