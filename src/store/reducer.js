846097006452b08eedbaa0f72be84ab3f65697dd
Yan Zhuk <yan.zhuk45@gmail.com>
2018-07-11 10:10:22 -0400

    add action and reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,41 @@
 const initialState = {
-  counter: 0
+  counter: 0,
+  results: []
 }
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      const newState = Object.assign({}, state)
+      newState.counter = state.counter + 1
+      return newState;
+    case 'DECREMENT':
+      return {
+        ...state,
+        counter: state.counter - 1
+      }
+    case 'ADD':
+      return {
+        ...state,
+        counter: state.counter + action.value}
+    case 'SUBTRACT':
+      return {
+        ...state,
+        counter: state.counter - action.value}
+    case 'STORE_RESULT':
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: state.counter })
+      }
+    case 'DELETE_RESULT':
+      const updatedArray = state.results.filter(result => result.id !== action.resultElId)
+      return {
+        ...state,
+        results: updatedArray
+      }
+    }
   return state;
 }
+
 
 export default reducer;
